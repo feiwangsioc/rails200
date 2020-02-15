5ddf579b66784cfc8de0eb0eb433d66cc108ae49
Fei Wang <feiwangsioc@Feis-MBP.fios-router.home>
2020-02-15 10:43:04 -0500

    post pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,64 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
+  end
+
+
+  def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+    # find_group_and_check_permission
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+    if @group.save
+
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    # find_group_and_check_permission
+    if @group.update(group_params)
+
+      redirect_to groups_path, notice: "update success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # find_group_and_check_permission
+    @group.destroy
+    flash[:alter] = "Group deleted"
+    redirect_to groups_path
+  end
+
+  private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :groups
-  root 'welcome#index'
+  devise_for :users
+  resources :groups do
+    resources :posts
+  end
+  root 'groups#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
